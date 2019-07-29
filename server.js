9812450298fd040b5b87bc52866c1a5fdb3db290
Marcelo Laos <mlaos1997@gmail.com>
2019-07-29 17:54:23 -0400

    server and index setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const registerRouter = require('../routes/register-router.js');
+const loginRouter = require('../routes/login-router.js');
+const usersRouter = require('../routes/users-router.js');
+
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/auth/signup', registerRouter);
+server.use('/api/auth/login', loginRouter);
+server.use('/api/auth/users', usersRouter);
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+});
+  
+  module.exports = server;
