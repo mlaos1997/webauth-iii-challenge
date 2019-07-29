0bca41f8d2ab32160e17a83d94888daf12a89f23
Marcelo Laos <mlaos1997@gmail.com>
2019-07-29 18:49:27 -0400

    added dbConfig, dbHelpers, and migration for users

---
+++ database/dbHelpers.js
@@ -0,0 +1,19 @@
+const db = require('./dbConfig.js');
+
+module.exports = {
+    register,
+    findBy,
+    get
+};
+
+function register(newUser) {
+    return db('users').insert(newUser);
+};
+
+function findBy(username) {
+    return db('users').where({ username }).first();
+};
+
+function get() {
+    return db('users');
+};
